e47c4829008dce2678b19c9ce0e344301d552a59
dlsqja <dlsqja3436@gmail.com>
2024-06-13 16:50:28 +0900

    fix : 모든 출처 허용

--- src/main/java/com/se/demo/global/WebConfig.java
+++ src/main/java/com/se/demo/global/WebConfig.java
@@ -8,12 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    /*
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("*")
                 .allowedOrigins("*");}
 
-     */
+
 }
